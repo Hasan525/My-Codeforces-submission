7c6e3b6da0f99d19f518007ba1bde84203a9816c
Hasan525 <rahmanhasanur715@gmail.com>
2022-01-21 22:09:28 +0600

    Apr/12/2021

---
+++ Contest/Educational Codeforces Round 107 (Rated for Div. 2)/C. Yet Another Card Deck.cpp
@@ -0,0 +1,120 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef unsigned long long int ull;
+#define vi vector<ll>
+#define vpi vector<pair<ll,ll> >
+#define mpi map<ll,ll>
+#define mpm map<vector<ll>,ll>
+#define  si set<ll>
+#define pqi  priority_queue<pair<ll, ll> >
+#define rep(i,a,n) for(ll i=a;i<n;i++)
+#define reP(i,a,n) for(ll i=a;i<=n;i++)
+#define YES cout<<"YES\n";
+#define NO cout<<"NO\n";
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define st string
+#define sz size()
+#define case(i,n) Case<<" "<<i<<": "<<n<<endl;
+#define tc ll t;cin>>t;while(t--)
+//#define mx -10000000000000;
+#define EMPTY_VALUE -1
+vector<ll>v;
+ll fact(ll x)
+{
+    ll ans=1;
+    for(ll i=2; i<=x; i++)
+        ans*=i;
+    return ans;
+}
+ll combinatorics(ll n,ll r)
+{
+    ll ans=1;
+    for(ll k=1; k<=r; k++)
+        ans=ans*(n+1-k)/k;
+    return ans;
+}
+void sieve(ll x)
+{
+    bool prime[x+2];
+    memset(prime,true,sizeof(prime));
+    for(ll i=2; i*i<=x; i++)
+    {
+        if(prime[i])
+        {
+            for(ll j=i*2; j<=x; j+=i)
+                prime[j]=false;
+        }
+    }
+    for(ll i=2; i<=x; i++)
+    {
+        if(prime[i])
+            v.pb(i);
+    }
+}
+bool compare(const pair<ll,ll>&a, const pair<ll,ll>&b)
+{
+    return a.first>b.first;
+}
+void divisors(ll x)
+{
+    for(ll i=1; i*i<=x; i++)
+    {
+        if(x%i==0)
+        {
+            v.pb(i);
+            if(i*i!=x)
+                v.pb(x/i);
+        }
+    }
+}
+/* ios::sync_with_stdio(false);
+    cin.tie();
+    cout.tie();*/
+
+
+
+
+
+int main()
+{
+        mpi mp;
+        ll n,m;cin>>n>>m;
+        ll ara[n];
+        for(ll i=0;i<n;i++)
+        {
+            cin>>ara[i];
+            if(mp[ara[i]]==0)
+            {
+                mp[ara[i]]=i+1;
+                v.pb(ara[i]);
+            }
+        }
+       ll x;
+        while(m--)
+        {
+            cin>>x;
+           cout<<mp[x]<<" ";
+           ll i=0;
+           ll cnt=v[i];
+           while(cnt!=x)
+           {
+               ll res=v[i+1];
+               v[i+1]=cnt;
+               mp[cnt]++;
+               cnt=res;
+               i++;
+           }
+           v[0]=cnt;
+           mp[cnt]=1;
+           /*for(ll i=0;i<v.sz;i++)
+            cout<<v[i]<<" ";
+           cout<<endl;*/
+
+        }
+        v.clear();
+
+}
